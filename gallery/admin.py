c44bd87c23cd79e5a230a9015bdc1a9541ec1721
pycat <kkampardi@gmail.com>
2017-07-17 10:06:17 +0300

    Add categories #11
    - add category model
    - relate it with gallery
    - add admin interface for category

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Gallery, GalleryImage
+from .models import Gallery, GalleryImage, Category
 
 # Register your models here.
 
@@ -8,11 +8,14 @@ from .models import Gallery, GalleryImage
 class GalleryImageInline(admin.StackedInline):
     model = GalleryImage
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',) }
+
 
 class GalleryAdmin(admin.ModelAdmin):
     inlines = [GalleryImageInline, ]
     prepopulated_fields = {'slug': ('title',)}
 
 
-
 admin.site.register(Gallery, GalleryAdmin)
+admin.site.register(Category, CategoryAdmin)
--- gallery/models.py
+++ gallery/models.py
@@ -1,15 +1,36 @@
 from django.db import models
+
 from filer.fields.image import FilerImageField
 from django.core.urlresolvers import reverse
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+
+    def get_absolute_url(self):
+        return reverse('category_list',args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
 
 class Gallery(models.Model):
     title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, related_name='works', default=1)
     slug = models.SlugField(unique=True)
-
     featured_image = FilerImageField(related_name="featured")
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name = "gallery"
+        verbose_name_plural = "Galleries"
 
     @models.permalink
     def get_absolute_url(self):
@@ -19,8 +40,6 @@ class Gallery(models.Model):
         return self.title
 
 
-
 class GalleryImage(models.Model):
     image_file = FilerImageField(related_name='gallery_images')
     gallery = models.ForeignKey(Gallery)
-    
